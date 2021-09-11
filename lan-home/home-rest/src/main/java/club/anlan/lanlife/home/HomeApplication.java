af6a9904d36a2d6a2c20191252b4aecd1381ed03
anlan <anlan.lhy@gmail.com>
2021-09-11 17:43:36 +0800

    :hammer: 修改为 home 模块

---
+++ lan-home/home-rest/src/main/java/club/anlan/lanlife/home/HomeApplication.java
@@ -0,0 +1,39 @@
+package club.anlan.lanlife.home;
+
+import club.anlan.lanlife.base.spring.AppContext;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * home 启动类
+ *
+ * @author lan
+ * @version 1.0
+ * @date 2021/9/11 17:25
+ */
+@SpringBootApplication
+@EnableAsync
+@ServletComponentScan(basePackages = {"club.anlan.lanlife"})
+@ComponentScan(basePackages = "club.anlan.lanlife")
+@EnableFeignClients("club.anlan.lanlife")
+@MapperScan(basePackages = {"club.anlan.lanlife.home.mapper"})
+public class HomeApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HomeApplication.class);
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(HomeApplication.class, args);
+        AppContext.setContext(applicationContext, true);
+    }
+}
