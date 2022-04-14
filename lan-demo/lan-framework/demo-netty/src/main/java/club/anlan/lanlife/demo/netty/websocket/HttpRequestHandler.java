69bd41a6a72f648fa15d931cda13820b72135038
anlan <anlan.lhy@gmail.com>
2022-04-14 23:47:30 +0800

    :deer: netty webSocket demo

---
+++ lan-demo/lan-framework/demo-netty/src/main/java/club/anlan/lanlife/demo/netty/websocket/HttpRequestHandler.java
@@ -0,0 +1,87 @@
+package club.anlan.lanlife.demo.netty.websocket;
+
+import io.netty.channel.*;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.ssl.SslHandler;
+import io.netty.handler.stream.ChunkedNioFile;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+/**
+ * HttpRequestHandler
+ *
+ * @author lan
+ * @version 1.0
+ * @date 2022/4/14 22:50
+ */
+public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+
+    private final String wsUri;
+
+    private static final File INDEX;
+
+    static {
+        URL location = HttpRequestHandler.class
+                .getProtectionDomain()
+                .getCodeSource().getLocation();
+        try {
+            String path = location.toURI() + "index.html";
+            path = !path.contains("file:") ? path : path.substring(5);
+            INDEX = new File(path);
+        } catch (URISyntaxException e) {
+            throw new IllegalStateException("unable to locate index.html", e);
+        }
+    }
+
+    public HttpRequestHandler(String wsUri) {
+        this.wsUri = wsUri;
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest request) throws Exception {
+        if (wsUri.equalsIgnoreCase(request.getUri())) {
+            // 增加引用计数
+            ctx.fireChannelRead(request.retain());
+        } else {
+            if (HttpHeaders.is100ContinueExpected(request)) {
+                send100Continue(ctx);
+            }
+            RandomAccessFile file = new RandomAccessFile(INDEX, "r");
+            HttpResponse response = new DefaultHttpResponse(request.getProtocolVersion(), HttpResponseStatus.OK);
+            response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/plain; charset=UTF-8");
+            boolean keepAlive = HttpHeaders.isKeepAlive(request);
+            if (keepAlive) {
+                response.headers().set(HttpHeaders.Names.CONTENT_LENGTH, file.length());
+                response.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+            }
+            ctx.write(response);
+            if (ctx.pipeline().get(SslHandler.class) == null) {
+                ctx.write(new DefaultFileRegion(file.getChannel(), 0, file.length()));
+            } else {
+                ctx.write(new ChunkedNioFile(file.getChannel()));
+            }
+
+            // 写LastHttpContent 并冲刷至客户端
+            ChannelFuture future = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+
+            //如果没有请求keep-alive， 则在写操作完成后关闭 Channel
+            if (!keepAlive) {
+                future.addListener(ChannelFutureListener.CLOSE);
+            }
+        }
+    }
+
+    private static void send100Continue(ChannelHandlerContext ctx) {
+        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.CONTINUE);
+        ctx.writeAndFlush(response);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
